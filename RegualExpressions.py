0e1a59b22571e9e94f358556e9a64efcba8675a9
unknown <shashankva05@gmail.com>
2026-07-30 10:27:23 +0530

    More RE

--- RegualExpressions.py
+++ RegualExpressions.py
@@ -1,3 +1,4 @@
+#RE Search
 import re
 string = "The rain in Spain"
 res = re.search("The", string)
@@ -5,3 +6,21 @@ if res:
     print("Sikthu Guru")
 else:
     print("Manege hogi malko.")
+
+
+#RE Search 2
+text= "The rain in Spain"
+if re.match("The", text):
+    print("Match found")
+else:
+    print("No match found")
+
+#RE Match gives all specific pattern in list
+data = "cat bat rat mat"
+res = re.findall("at", data)
+print("All occurances of 'at': ", res)
+
+#RE Substituiton
+text = "I like python programming"
+res = re.sub("python", "Java", text)
+print("After Substitution:", res)
