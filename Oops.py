bdcec0f23683590436643f296cffebd9f8e69cea
unknown <shashankva05@gmail.com>
2026-07-31 09:21:45 +0530

    OOPS UPDATE

--- Oops.py
+++ Oops.py
@@ -1,3 +1,4 @@
+#Program 1
 class Student:
     name = "Shashank VA"
     age = 21
@@ -6,9 +7,28 @@ print("Name:", s.name)
 print("Age:", s.age)
 
 
-
+#Program 2
 class Student:
     def display(self):
         print("This is a student class")
 s = Student()
 s.display()
+
+
+#Program 3
+class Student:
+    college = "MVJ College Of Engineering" #Class Variable
+    def __init__(self,name):
+        self.name = name   #Instance Variable
+        
+#Parameterised Constructor
+s1 = Student("Swarup")
+s2 = Student("Rahul")
+print(s1.name)
+print(s2.name)
+print(s1.college)
+print(s2.college)
+
+Student.college = "IISC"
+print(s1.college)
+print(s2.college)
